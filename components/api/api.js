8d71d79870021a76695a75411dfb3ffb83c00dc7
Ivonilson <ivonilsoncardoso@hotmail.com>
2024-09-05 08:43:23 -0300

    update

---
+++ components/api/api.js
@@ -0,0 +1,11 @@
+function login() {
+    const login = 'https://abgsolucoes.tec.br/api/login.php'
+    return login
+}
+
+function produtos(){
+    const produtos = 'https://abgsolucoes.tec.br/api/produtos.php'
+    return produtos
+}
+
+module.exports = {login, produtos}
